11c9fe509a88935dbbf9555c07eec67ab56551f6
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 16:47:11 +0200

    Create StartUp.cs

---
+++ 01. Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog doggie = new Dog();
+            doggie.Eat();
+            doggie.Bark();
+        }
+    }
+}
