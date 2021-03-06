7eb031a340415f249a43bf4aee246241c4c57bbb
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 20:22:53 +0200

    Add files via upload

---
+++ 04. Encapsulation - Exercise/03. Shopping Spree/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+   public static class Validator
+    {
+        public static void ThrowIfStringIsNullOrEmpty(string str, string exMessage)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(exMessage);
+            }
+        }
+
+        public static void ThrowIfNumberIsNegative(decimal number, string exMessage)
+        {
+            if (number <0)
+            {
+                throw new ArgumentException(exMessage);
+            }
+        }
+    }
+}
