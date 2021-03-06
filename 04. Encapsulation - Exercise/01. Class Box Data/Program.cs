c9978273801d1a246c0112dcdf955e4c425bbec0
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 19:19:44 +0200

    Create Program.cs

---
+++ 04. Encapsulation - Exercise/01. Class Box Data/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, heigth);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
