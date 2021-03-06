95479b13679acffa51bfe17f4ee3edbe47eb24fe
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-06 17:40:20 +0200

    Create StartUp.cs

---
+++ 02. Inheritance - Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
