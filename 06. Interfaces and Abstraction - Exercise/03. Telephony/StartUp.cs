8cfa22dc56d8fa511032cba08cd4b307e4727622
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-07 16:22:43 +0200

    Create StartUp.cs

---
+++ 06. Interfaces and Abstraction - Exercise/03. Telephony/StartUp.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            string[] urls = Console.ReadLine().Split();
+
+            Smartphone smartPhone = new Smartphone();
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+
+            foreach (var number in numbers)
+            {
+                try
+                {
+                    string result = number.Length == 10
+                                    ? smartPhone.Call(number)
+                                    : stationaryPhone.Call(number);
+
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    string result = smartPhone.Browse(url);
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+              
+            }
+        }
+    }
+}
