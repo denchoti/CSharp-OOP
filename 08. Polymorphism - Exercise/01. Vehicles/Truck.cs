7ba713ea614599f2e15a601b3fed780c6e2bfaff
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-08 22:17:19 +0200

    Add files via upload

---
+++ 08. Polymorphism - Exercise/01. Vehicles/Truck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+   public class Truck : Vehicle
+    {
+        private const double TruckAirConditionerModifer = 1.6;
+
+        public Truck(double fuel, double fuelConsumption) : base(fuel, fuelConsumption, TruckAirConditionerModifer)
+        {
+        }
+
+        public override void Refuel(double amount)
+        {
+            base.Refuel(amount * 0.95);
+        }
+    }
+}
