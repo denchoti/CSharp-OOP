be5b6cca1bc5b1cf7097e32c15f923255df650fd
Denitsa <65463547+denchoti@users.noreply.github.com>
2021-03-23 21:47:33 +0200

    Add files via upload

---
+++ 05. Interfaces and Abstraction/Shapes/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Rectangle : IDrawable
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        private int height;
+        private int width;
+        public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 1; i < this.Height - 1; ++i)
+           
+                DrawLine(this.Width, '*', ' ');
+            DrawLine(this.Width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+
+    }
+}
